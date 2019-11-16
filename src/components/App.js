0ecdb60e8fbcd8964474392e67a4a5b9995d72c4
Adam Koller <adamkoller@Adams-MacBook-Pro.local>
2019-11-15 22:11:23 -0600

    changed function components to class components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      this is the start of my ecoommerce store
+    </div>
+  );
+}
+
+export default App;
+const root = document.getElementById('root');
+ReactDOM.render(<App />,root);
