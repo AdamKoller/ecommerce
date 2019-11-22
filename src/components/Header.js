e74cb826fd9a6af98937a0828dd0e84956331082
Adam Koller <adamkoller.86@gmail.com>
2019-11-21 21:11:12 -0600

    product title position

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,23 +21,34 @@ render () {
           <a href="#"><i className="fa fa-shopping-cart"></i></a>
         </div>
       </header>
+
       <div className='spacer'></div>
+
       <div className='free-shipping'>
         Free Shipping On All Orders <span className='line'>|</span> Buy Now Pay Later With <span className='klarna'>Klarna</span>
       </div>
+
       <section className="content-area-product">
         <div className="top-section">
           <div className="product-wrapper">
-            <div className="product-bubble">
-              <img src="https://drummersonly.co.uk/wp-content/uploads/2018/10/yf96S1Ug-800x800.jpeg"/>
-            </div>
+            <img src="https://www.kickstartyourdrumming.com/wp-content/uploads/2017/10/51t5fnYm88L.jpg"/>
+          </div>
+          <div className="product-title">
+            <h3> Tama </h3>
+            <h1> Imperialstar </h1>
+          </div>
+        </div>
+
+        <div className="top-section">
+          <div className="product-wrapper">
+            <img src="https://www.cahousemusic.com/images/product/large/ae00-8577.jpg"/>
+          </div>
+          <div className="product-title">
+            <h3> Mapex </h3>
+            <h1> Stage Custom </h1>
           </div>
         </div>
       </section>
-      <div className="product-title">
-        <h3> Tama </h3>
-        <h1> Starclassic </h1>
-      </div>
     </div>
   );
 }
