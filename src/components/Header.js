bc78e06cc06e38c0b747ec20f241a9a45c4b72d6
Adam Koller <adamkoller.86@gmail.com>
2019-11-26 22:48:23 -0600

    total items

--- src/components/Header.js
+++ src/components/Header.js
@@ -47,26 +47,82 @@ render () {
           </div>
         </div>
         <div className='cart-items'>
-          <div className='item'>
-            <img src='https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png'/>
-            <div className='item-delete'>
-              <div className='delete-button'>
-                X
+          <div className='item-wrapper'>
+            <div className='item'>
+              <img src='https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png'/>
+              <div className='item-delete'>
+                <div className='delete-button'>
+                  X
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='item-wrapper'>
+            <div className='item'>
+              <img src='https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png'/>
+              <div className='item-delete'>
+                <div className='delete-button'>
+                  X
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='item-wrapper'>
+            <div className='item'>
+              <img src='https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png'/>
+              <div className='item-delete'>
+                <div className='delete-button'>
+                  X
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='item-wrapper'>
+            <div className='item'>
+              <img src='https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png'/>
+              <div className='item-delete'>
+                <div className='delete-button'>
+                  X
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='item-wrapper'>
+            <div className='item'>
+              <img src='https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png'/>
+              <div className='item-delete'>
+                <div className='delete-button'>
+                  X
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='item-wrapper'>
+            <div className='item'>
+              <img src='https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png'/>
+              <div className='item-delete'>
+                <div className='delete-button'>
+                  X
+                </div>
               </div>
             </div>
           </div>
         </div>
+
         <div className='cart-product-total'>
-        <div className='cart-price-total-title'>Total</div>
-        <div className='cart-price-total-price'>x4</div>
+          <div className='cart-product-title'>Total</div>
+          <div className='cart-product-number'>x4</div>
         </div>
+
         <div className='cart-price-total'>
           <div className='cart-price-total-title'>Total</div>
           <div className='cart-price-total-price'>$750</div>
         </div>
+
         <div className='cart-checkout'>
           Checkout
         </div>
+        
       </section>
     </div>
   );
