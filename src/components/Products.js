1a3d73ed898bbd33594e737457f4f473668d58a5
Adam Koller <adamkoller.86@gmail.com>
2019-12-08 15:44:06 -0600

    filter/products layout

--- src/components/Products.js
+++ src/components/Products.js
@@ -12,7 +12,34 @@ class Products extends Component {
 render () {
   return (
       <div>
-        
+        <section className='products-page-wrapper'>
+          <div className='products-filter'>
+            filter
+          </div>
+          <div className='products-grid'>
+            <div className="products-wrapper">
+              <img src="https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png" alt='drums'/>
+            </div>
+            <div className="products-title">
+              <h3> Tama </h3>
+              <h1> Imperialstar </h1>
+            </div>
+            <div className="products-wrapper">
+              <img src="https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png" alt='drums'/>
+            </div>
+            <div className="products-title">
+              <h3> Tama </h3>
+              <h1> Imperialstar </h1>
+            </div>
+            <div className="products-wrapper">
+              <img src="https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png" alt='drums'/>
+            </div>
+            <div className="products-title">
+              <h3> Tama </h3>
+              <h1> Imperialstar </h1>
+            </div>
+          </div>
+        </section>
       </div>
   );
 }
