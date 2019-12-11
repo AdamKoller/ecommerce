668f274a340dc53c735d19e5e214d4ae7c049af3
Adam Koller <adamkoller.86@gmail.com>
2019-12-10 21:33:09 -0600

    work on products page

--- src/components/Products.js
+++ src/components/Products.js
@@ -12,31 +12,20 @@ class Products extends Component {
 render () {
   return (
       <div>
-        <section className='products-page-wrapper'>
+        <section className='products-page-wrapper'>{/*content area*/}
           <div className='products-filter'>
             filter
           </div>
-          <div className='products-grid'>
-            <div className="products-wrapper">
-              <img src="https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png" alt='drums'/>
-            </div>
-            <div className="products-title">
-              <h3> Tama </h3>
-              <h1> Imperialstar </h1>
-            </div>
-            <div className="products-wrapper">
-              <img src="https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png" alt='drums'/>
-            </div>
-            <div className="products-title">
-              <h3> Tama </h3>
-              <h1> Imperialstar </h1>
-            </div>
-            <div className="products-wrapper">
-              <img src="https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png" alt='drums'/>
-            </div>
-            <div className="products-title">
-              <h3> Tama </h3>
-              <h1> Imperialstar </h1>
+
+          <div className='products-grid'> {/*top section*/}
+            <div className='products-wrapper'>
+              <div className="products-image">
+                <img src="https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png" alt='drums'/>
+              </div>
+              {/*<div className="products-title">
+                <h3> Tama </h3>
+                <h1> Imperialstar </h1>
+              </div>*/}
             </div>
           </div>
         </section>
