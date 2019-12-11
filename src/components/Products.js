43f843f5727b08ad33e0b11f1455bf695bc2df37
Adam Koller <adamkoller.86@gmail.com>
2019-12-10 21:56:51 -0600

    work

--- src/components/Products.js
+++ src/components/Products.js
@@ -22,11 +22,36 @@ render () {
               <div className="products-image">
                 <img src="https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png" alt='drums'/>
               </div>
-              {/*<div className="products-title">
-                <h3> Tama </h3>
-                <h1> Imperialstar </h1>
-              </div>*/}
+              <div className="title">
+                <h3> Tama Imperialstar</h3>
+              </div>
+              <div className="price">
+                <h3>$899</h3>
+              </div>
             </div>
+            <div className='products-wrapper'>
+              <div className="products-image">
+                <img src="https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png" alt='drums'/>
+              </div>
+              <div className="title">
+                <h3> Tama Imperialstar</h3>
+              </div>
+              <div className="price">
+                <h3>$899</h3>
+              </div>
+            </div>
+            <div className='products-wrapper'>
+              <div className="products-image">
+                <img src="https://www.grahamrusselldrums.com/media/catalog/product/cache/1/small_image/500x/9df78eab33525d08d6e5fb8d27136e95/t/a/tama_star_bubinga_smokey_black_set.png" alt='drums'/>
+              </div>
+              <div className="title">
+                <h3> Tama Imperialstar</h3>
+              </div>
+              <div className="price">
+                <h3>$899</h3>
+              </div>
+            </div>
+            
           </div>
         </section>
       </div>
